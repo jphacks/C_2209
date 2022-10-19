b8a026b055318fcba98af5044dbf96e4e6cce8d8
imanishi03 <imasa@toralab.org>
2022-10-19 18:47:27 +0900

    パワポ

--- src/powerpoint.py
+++ src/powerpoint.py
@@ -1,36 +1,25 @@
 import collections.abc
 from operator import imod # インポートしないとエラーが発生する
-from pptx.util import Inches  # インチ
 import pptx
-
-from pptx.enum.shapes import MSO_SHAPE
+from pptx.enum.shapes import MSO_SHAPE    # 図形の定義がされているクラス
 from pptx.dml.color import RGBColor
-from pptx.util import Cm,Pt               # 単位指定をするクラス(センチメートル, ポイント単位)
+from pptx.util import Cm,Pt,Inches              # 単位指定をするクラス(センチメートル, ポイント単位)
 import os
 from PIL import Image 
+from pptx.enum.text import PP_ALIGN       # 段落の水平位置のEnume
 
-'''
-配列outputにoutput_func5にある画像追加 
-→ test.pyで画像出力するときに連番になるように変えていいなら、ここのコードはいらない
-→ でもファイル名変えるとどの部分の画像か分からなくなって、改良しにくくなるかも
-'''
-output = []
-num = 0
-while True:  
-    image_path = f'./output_func5/output1_{num:04d}.jpeg'
-    if os.path.exists(image_path):
-        output.append(image_path)
-    num += 1
-    if not os.path.exists(f'./output/output1_{num:04d}.jpeg'):
-        break
+#画像の格納ディレクトリ
+IMG_DIR = "./output_func5/"
+#img画像のファイル名を取得
+img_names = os.listdir(IMG_DIR)
+img_names = [name for name in img_names if name.endswith(".jpeg")]
 
 prs = pptx.Presentation()
 prs.slide_width = Inches(11.69) #A4サイズ
 prs.slide_height = Inches(8.27)
-slide = prs.slides.add_slide(prs.slide_layouts[6])  # 空白のスライドを追加
 
 #画像のアスペクト比を取得
-im = Image.open('./output/output1_0000.jpeg')
+im = Image.open(IMG_DIR + img_names[0])
 aspect_ratio = im.width /im.height
 
 #画像の高さの設定と幅の取得
@@ -39,43 +28,74 @@ pic_width = aspect_ratio * pic_height
 
 
 #画像を１枚のパワポに出力 1段4枚ずつ
-pic_top = Cm(0.5)
 pic_left = Cm(2.5)
 pic_left2 = pic_left + pic_width
-for i in range(len(output)):
-    image = slide.shapes.add_picture(output[i], pic_left, pic_top, height=pic_height)
+for i, name in enumerate(img_names):
+    path = IMG_DIR + name
+
+    if i % 8 == 0:
+        slide = prs.slides.add_slide(prs.slide_layouts[6])
+        pic_top = Cm(0.5)
+
+    #画像追加
+    image = slide.shapes.add_picture(path, pic_left, pic_top, height=pic_height)
     image.line.color.rgb = RGBColor(0, 0, 0)
     image.line.width = Pt(1.5)
+    
+    #テキストを追加
+    width = height = Pt(28)
+    textbox = slide.shapes.add_textbox(pic_left-Cm(1), pic_top, width, height)
+    tf = textbox.text_frame
+    tf.text = str(i)
+    tf.paragraphs[0].font.size = Pt(28)  # font size
+    #tf.paragraphs[0].font.bold = True  # font bold
+    tf.paragraphs[0].alignment = PP_ALIGN.RIGHT
+
     pic_left += Cm(7)
+
     if i % 4 == 3:
         pic_top += Cm(10)
         pic_left = Cm(2.5)
         pic_left2 = pic_left + pic_width
         pic_top2 += Cm(11)
-    elif i != len(output)-1:
+    elif i != len(img_names)-1:
+
     #矢印出力
         pic_top2 = pic_top + pic_height/2 - Cm(1)
         rect0 = slide.shapes.add_shape(		# shapeオブジェクト➀を追加
                 MSO_SHAPE.RIGHT_ARROW,   	                    # 図形の種類を[丸角四角形]に指定
                 pic_left2, pic_top2,               # 挿入位置の指定：左からの座標と上からの座標の指定
                 Cm(7) - pic_width, Cm(2))               # 挿入図形の幅と高さの指定
-        pic_left2 += Cm(7)
+        pic_left2 += Cm(7) 
 
 #画像を２枚ずつパワポに出力
 pic_height = Cm(16)
 pic_width = aspect_ratio * pic_height
 slide_width = prs.slide_width
 slide_height = prs.slide_height
-
 pic_top = ( slide_height - pic_height ) / 2
-for i in range(len(output)):
+
+for i, name in enumerate(img_names):
+    path = IMG_DIR + name
+
     if i % 2 == 0:
         slide = prs.slides.add_slide(prs.slide_layouts[6]) 
         pic_left = ( slide_width/2 - pic_width ) / 2
     else:
         pic_left += slide_width/2
-    image = slide.shapes.add_picture(output[i], pic_left, pic_top, height=pic_height)   
+
+    #画像を追加
+    image = slide.shapes.add_picture(path, pic_left, pic_top, height=pic_height)   
     image.line.color.rgb = RGBColor(0, 0, 0)
     image.line.width = Pt(1.5)
 
+    #テキストを追加
+    width = height = Pt(36)
+    textbox = slide.shapes.add_textbox(pic_left-Cm(1.5), pic_top, width, height)
+    tf = textbox.text_frame
+    tf.text = str(i)
+    tf.paragraphs[0].font.size = Pt(36)  # font size
+    #tf.paragraphs[0].font.bold = True  # font bold
+    tf.paragraphs[0].alignment = PP_ALIGN.RIGHT
+    
 prs.save("./test.pptx")
