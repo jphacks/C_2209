0ec9ff779349176e924cfd8be771b450761789df
imanishi03 <imasa@toralab.org>
2022-10-20 17:02:03 +0900

    パワポ

--- src/powerpoint.py
+++ src/powerpoint.py
@@ -29,7 +29,7 @@ pic_width = aspect_ratio * pic_height
 
 #画像を１枚のパワポに出力 1段4枚ずつ
 pic_left = Cm(2.5)
-pic_left2 = pic_left + pic_width
+pic_left2 = Cm(7.2)
 for i, name in enumerate(img_names):
     path = IMG_DIR + name
 
@@ -46,9 +46,8 @@ for i, name in enumerate(img_names):
     width = height = Pt(28)
     textbox = slide.shapes.add_textbox(pic_left-Cm(1), pic_top, width, height)
     tf = textbox.text_frame
-    tf.text = str(i)
+    tf.text = str(i+1)
     tf.paragraphs[0].font.size = Pt(28)  # font size
-    #tf.paragraphs[0].font.bold = True  # font bold
     tf.paragraphs[0].alignment = PP_ALIGN.RIGHT
 
     pic_left += Cm(7)
@@ -56,7 +55,7 @@ for i, name in enumerate(img_names):
     if i % 4 == 3:
         pic_top += Cm(10)
         pic_left = Cm(2.5)
-        pic_left2 = pic_left + pic_width
+        pic_left2 = Cm(7.2)
         pic_top2 += Cm(11)
     elif i != len(img_names)-1:
 
@@ -65,7 +64,9 @@ for i, name in enumerate(img_names):
         rect0 = slide.shapes.add_shape(		# shapeオブジェクト➀を追加
                 MSO_SHAPE.RIGHT_ARROW,   	                    # 図形の種類を[丸角四角形]に指定
                 pic_left2, pic_top2,               # 挿入位置の指定：左からの座標と上からの座標の指定
-                Cm(7) - pic_width, Cm(2))               # 挿入図形の幅と高さの指定
+                Cm(2), Cm(2))               # 挿入図形の幅と高さの指定
+        rect0.fill.solid()                                   # shapeオブジェクト➀を単色で塗り潰す
+        rect0.fill.fore_color.rgb = RGBColor(74, 126, 187)  # RGB指定で色を指定
         pic_left2 += Cm(7) 
 
 #画像を２枚ずつパワポに出力
@@ -93,9 +94,19 @@ for i, name in enumerate(img_names):
     width = height = Pt(36)
     textbox = slide.shapes.add_textbox(pic_left-Cm(1.5), pic_top, width, height)
     tf = textbox.text_frame
-    tf.text = str(i)
+    tf.text = str(i+1)
     tf.paragraphs[0].font.size = Pt(36)  # font size
-    #tf.paragraphs[0].font.bold = True  # font bold
     tf.paragraphs[0].alignment = PP_ALIGN.RIGHT
-    
+
+    #矢印出力
+    ratio = 0.45
+    pic_left2 = slide_width/2 - pic_width*ratio/2
+    pic_top2 = slide_height/2 - pic_width*ratio/2
+    rect0 = slide.shapes.add_shape(		# shapeオブジェクト➀を追加
+            MSO_SHAPE.RIGHT_ARROW,   	                    # 図形の種類を[丸角四角形]に指定
+            pic_left2, pic_top2,               # 挿入位置の指定：左からの座標と上からの座標の指定
+            pic_width*ratio, pic_width*ratio)               # 挿入図形の幅と高さの指定
+    rect0.fill.solid()                                   # shapeオブジェクト➀を単色で塗り潰す
+    rect0.fill.fore_color.rgb = RGBColor(74, 126, 187)  # RGB指定で色を指定
+
 prs.save("./test.pptx")
